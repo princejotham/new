ddf79f203d6c7c53350b41bf6dacd0ede3e3edc3
prince <princejomino@gmail.com>
2020-11-27 23:48:37 +0800

    Add Forms

--- application/modules/research/views/create.php
+++ application/modules/research/views/create.php
@@ -1,19 +1,144 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <div class="page-head">
 	<h4 class="my-2"><?=$page_title;?></h4>
-	<p>Input Title</p>
-	<p>Input Name of Proponent/s</p>
-	<p>Input College Affiliation</p>
-	<p>Input Background of the Study</p>
-	<p>Input Significant of the Study</p>
-	<p>Input Definition of Terms</p>
-	<p>Input Review of literature</p>
-	<p>Input Methodology</p>
-	<p>Input Work Plan (Gantt Chart)</p>
-	<p>Input Line Item Budget</p>
-	<p>Input Credentials of Key Personel/Staff Involved</p>
-	<p>Input References</p>
-	<p>Input Prepared By</p>
-	<button>Submit</button>
-	<button>Cancel</button>
 </div>
+<div class="row">
+    <div class="col-12">
+        <div class="card m-b-30">
+            <div class="card-body">
+                <h5 class="header-title pb-3">Fill Out the forms</h5>            
+                <div class="general-label">
+                    <form class="form-horizontal" role="form">
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">Title</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label"></label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label"></label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label"></label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">Significant of the Study</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">Definition of Terms</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">Review of literature</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">Methodology</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">Work Plan (Gantt Chart)</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">Work Plan (Gantt Chart)</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">Line Item Budget</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">Credentials of Key Personel/Staff Involved</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">References</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2 control-label">Prepared By</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" value="">
+                            </div>
+                        </div>
+						<button type="submit" class="btn btn-primary">Submit</button>
+                    </form>           
+                </div>                            
+            </div>
+        </div>
+    </div>
+</div><!--end row-->
+<div class="row">
+    <div class="col-12">
+        <div class="card m-b-30">
+            <div class="card-body">
+                <h5 class="header-title pb-3">Basic example</h5>           
+                <div class="general-label">
+                    <form role="form">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Title</label>
+                            <input type="text" class="form-control" value="" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Name of Proponent/s</label>
+                            <input type="text" class="form-control" value="" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">College Affiliation</label>
+                            <input type="text" class="form-control" value="" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Background of the Study</label>
+                            <input type="text" class="form-control" value="" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="text" class="form-control" value="" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="text" class="form-control" value="" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="text" class="form-control" value="" placeholder="">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>                                    
+                </div>                            
+            </div>
+        </div>
+    </div>
+</div><!--end row-->
